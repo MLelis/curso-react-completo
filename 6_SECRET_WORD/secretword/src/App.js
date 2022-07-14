63debfff4f28e53d5b28a8bc122d2d2774502334
Marcus Lelis <marcuslelis95@gmail.com>
2022-07-14 14:42:17 -0300

    Processando letras validas e invalidas

--- 6_SECRET_WORD/secretword/src/App.js
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -63,7 +63,27 @@ function App() {
 
   // process the latter input
   const verifyLetter = (letter) => {
-    console.log(letter);
+    
+    const normalizedLetter = letter.toLowerCase();
+
+    // check if letter has already been utilized
+    if ( guessedLetters.includes(normalizedLetter) || wrongLetters.includes(normalizedLetter) ) {
+      return ;
+    }
+
+    // push guessed letter or remove a guess
+    if(letters.includes(normalizedLetter)){
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter
+      ]);
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter
+      ]);
+    }
+
   }
 
   // restart the game
