b516ae06acd3113c149f92404d64319a38b25b46
Marcus Lelis <marcuslelis95@gmail.com>
2022-07-14 15:14:40 -0300

    Finalização do projeto

--- 6_SECRET_WORD/secretword/src/App.js
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -34,7 +34,7 @@ function App() {
   const [guesses, setGuesses] = useState(guessesQtd);
   const [score, setScore] = useState(0);
 
-  const pickWordAndCategory = () => {
+  const pickWordAndCategory = useCallback(() => {
     // pick a random category
     const categories = Object.keys(words);
     const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
@@ -43,10 +43,13 @@ function App() {
     const word = words[category][Math.floor(Math.random() * words[category].length)];
 
     return {word, category};
-  }
+  }, [words]);
 
   // Start the Secret Word Game
-  const startGame = () => {
+  const startGame = useCallback(() => {
+    // clear all letters
+    clearLetterStates();
+
     // pick word and pick category
     const { word, category } = pickWordAndCategory();
 
@@ -61,7 +64,7 @@ function App() {
     setLetters(wordLetters);
 
     setGameStage(stages[1].name);
-  }
+  }, [pickWordAndCategory]);
 
   // process the latter input
   const verifyLetter = (letter) => {
@@ -104,6 +107,22 @@ function App() {
     }
   }, [guesses])
 
+  // check win condition
+  useEffect(() => {
+
+    const uniqueLetters = [...new Set(letters)]
+
+    // win condition
+    if(guessedLetters.length === uniqueLetters.length) {
+      // add score
+      setScore((actualScore) => actualScore += 100 )
+
+      // restart game with new world
+      startGame();
+    }
+
+  }, [guessedLetters, letters, startGame])
+
   // restart the game
   const retry = () => {
     setScore(0);
@@ -126,7 +145,7 @@ function App() {
           score={score}
         />
       )}
-      {gameStage === 'end' && <GameOver retry={retry} />}
+      {gameStage === 'end' && <GameOver retry={retry} score={score} />}
     </div>
   );
 }
