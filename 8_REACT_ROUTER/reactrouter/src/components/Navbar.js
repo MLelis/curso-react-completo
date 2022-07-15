ace09125f050e445a3c43cd6a9cf2f932b507f4b
Marcus Lelis <marcuslelis95@gmail.com>
2022-07-15 11:06:34 -0300

    React Routes

---
+++ 8_REACT_ROUTER/reactrouter/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import "./Navbar.css";
+import { Link, NavLink } from "react-router-dom";
+
+// 2 - links com react router
+
+const Navbar = () => {
+  return (
+    <nav>
+        {/* <Link to="/">Home</Link>
+        <Link to="/about">Sobre</Link> */}
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">Sobre</NavLink>
+    </nav>
+  )
+}
+
+export default Navbar
